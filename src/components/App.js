9b6a1a211196b340c2852b41907f5d136962b242
ropaillet <ropaillet@gmail.com>
2018-05-29 18:29:36 -0300

    Test 3

--- src/components/App.js
+++ src/components/App.js
@@ -5,22 +5,13 @@ import Header from './Header';
 import Footer from './Footer';
 
 
-import { Switch, Route } from 'react-router-dom'
-import Home from './Home'
-import Ngos from './Ngos'
-
 const App = () => {
   return (
-    <Switch>
-      <Route exact path='/' component={Home}/>
-      <Route exact path='/ngos' component={Ngos}/>
-      <Route exact path='/abc' component={Ngos}/>
-    </Switch>
-    // <Grid>
-    //   <Header />
-    //   <Main />
-    //   <Footer />
-    // </Grid>
+    <Grid>
+      <Header />
+      <Main />
+      <Footer />
+    </Grid>
   )
 }
 
--- src/components/Main.js
+++ src/components/Main.js
@@ -10,8 +10,8 @@ const Main = () => {
       <Switch>
         <Route exact path='/' component={Home}/>
         <Route exact path='/ngos' component={Ngos}/>
-        <Route exact path='/platform' component={Home}/>
-        <Route exact path='/about' component={Home}/>
+        {/* <Route exact path='/platform' component={Home}/>
+        <Route exact path='/about' component={Home}/> */}
       </Switch>
     </MainDiv>
   )
